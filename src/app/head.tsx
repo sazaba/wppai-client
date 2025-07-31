65df50f7f0af9ad257bdcea38cae865b322bab8a
Santiago Zappa <santiagozappab@gmail.com>
2025-07-30 21:04:09 -0500

    fb-id fix

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
-    return (
-      <>
-        <meta property="fb:app_id" content="1491280195185816" />
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <meta property="fb:app_id" content="1491280195185816" />
+      <link rel="icon" href="/logo.webp" type="image/webp" />
+    </>
+  )
+}
